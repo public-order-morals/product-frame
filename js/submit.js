df7d36396d57782eb85c8c207f68a1c694e369c3
Taguwa <tagucraft@gmail.com>
2022-08-25 12:02:41 +0900

    user_menu , data_upload_server

--- js/submit.js
+++ js/submit.js
@@ -1,27 +1,27 @@
-
+var str1,str2,str3,str4,str5,str6,str7,str8;
 function submit_input() {
-    const str1 = document.getElementById("submit_kind").value;
+    str1 = document.getElementById("submit_kind").value;
 
     console.log(str1);
-    const str2 = document.getElementById("submit_place").value;
+    str2 = document.getElementById("submit_place").value;
 
     console.log(str2);
-    const str3 = document.getElementById("submit_user").value;
+    str3 = document.getElementById("submit_user").value;
 
     console.log(str3);
-    const str4 = document.getElementById("submit_admin").value;
+    str4 = document.getElementById("submit_admin").value;
 
     console.log(str4);
-    const str5 = document.getElementById("submit_name").value;
+    str5 = document.getElementById("submit_name").value;
 
     console.log(str5);
-    const str6 = document.getElementById("submit_format").value;
+    str6 = document.getElementById("submit_format").value;
 
     console.log(str6);
-    const str7 = document.getElementById("submit_number").value;
+    str7 = document.getElementById("submit_number").value;
 
     console.log(str7);
-    const str8 = document.getElementById("submit_images").value;
+    str8 = document.getElementById("submit_images").value;
 
     console.log(str8);
 
@@ -38,4 +38,18 @@ function submit_input() {
 
 function upload_input(){
     message.innerHTML = "登録しました。"
+    data.push({
+        "id":"0",
+        "資産番号": "00000-99999999-00000",
+        "所属":str1,
+        "資産名":str5,
+        "場所": str2,
+        "担当":str3,
+        "管理者":str4,
+        "形式":str6,
+        "個数":str7,
+        "識別番号": "1",
+        "取得日時": "2022/07/12",
+        "編集日時": "2022/07/13"
+    });
 }
